5a7e69713153a8250eedf4fcda78647f8fbf0deb
Rishikesh Maddhesiya <rishi89324@gmail.com>
2025-01-17 00:55:07 +0530

    added cors

--- agent/my_agent/demo.py
+++ agent/my_agent/demo.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()  
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -13,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://plan-sarthi.vercel.app"],  # Vercel frontend domain
+    allow_origins=["https://plan-sarthi.vercel.app/"],  
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_credentials=True,
     allow_headers=["*"],
